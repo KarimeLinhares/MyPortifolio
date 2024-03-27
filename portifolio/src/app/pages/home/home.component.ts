4fe956de2ff347d48c61bf644051b617d26e55cf
KarimeLinhares <karime_muniz@hotmail.com>
2024-03-27 16:12:59 -0300

    fix in ts of back-to-top button

--- portifolio/src/app/pages/home/home.component.ts
+++ portifolio/src/app/pages/home/home.component.ts
@@ -1,39 +1,11 @@
-import {
-  Component,
-  ViewChildren,
-  QueryList,
-  AfterViewInit,
-  ChangeDetectorRef,
-  ElementRef,
-} from '@angular/core';
-import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
-import { BigcardComponent } from '../../components/bigcard/bigcard.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements AfterViewInit {
-  showBackToTop = false;
-  @ViewChildren(MenuBarComponent, { read: ElementRef }) menuBar!: QueryList<
-    ElementRef<MenuBarComponent>
-  >;
-  @ViewChildren(BigcardComponent, { read: ElementRef }) bigCard!: QueryList<
-    ElementRef<BigcardComponent>
-  >;
-
-  constructor(private cdr: ChangeDetectorRef) {}
-
-  ngAfterViewInit() {
-    this.cdr.markForCheck();
-    setTimeout(() => {
-      this.showBackToTop =
-        this.menuBar.toArray().length > 0 && this.bigCard.toArray().length > 0;
-      this.cdr.markForCheck();
-    });
-  }
-
+export class HomeComponent {
   scrollToTop() {
     window.scrollTo({
       top: 0,
